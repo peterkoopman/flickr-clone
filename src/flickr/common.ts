de0da85bcf83a893dc10c04aa20b21868c740cd4
Peter Koopman <peter@scribbledesign.co.nz>
2025-10-01 13:23:39 +1300

    use env vars to create flickr object

--- src/flickr/common.ts
+++ src/flickr/common.ts
@@ -1,3 +1,3 @@
 import { createFlickr } from "flickr-sdk";
-export const { flickr } = createFlickr("05bcf684d4158a40dd7ab3dd9c4a625b");
-export const userId: string = "11077827@N02";
+export const { flickr } = createFlickr(process.env.REACT_APP_FLICKR_API_KEY as string);
+export const userId: string | undefined = process.env.REACT_APP_FLICKR_USER_ID;
